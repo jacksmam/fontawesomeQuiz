8e9cc7b7a7dfa6c82353427258ec105a1ee8149e
jacksmam <jacksmam0@gmail.com>
2017-07-08 21:13:30 +0900

    tslint対応

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes }   from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { TopComponent } from './top/top.component';
 
 @NgModule({
